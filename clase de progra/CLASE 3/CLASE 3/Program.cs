62858ca648ae4700f2ec82eaf8a45e48290c4f56
MorenaJv28 <124007217+MorenaJv28@users.noreply.github.com>
2023-08-19 16:00:39 -0600

    Add files via upload

---
+++ clase de progra/CLASE 3/CLASE 3/Program.cs
@@ -0,0 +1,50 @@
+﻿// LISTA lEIVA <3
+
+//AGREGAR A LA LISTA
+//List <int> Edad  = new List<int>();
+//Edad.Add(-1);
+
+
+//foreach (int i in Edad)
+//{   
+
+//   Console.WriteLine(i);
+//}
+
+//Edad.Insert(0, 30);
+//Console.WriteLine("\n");
+//foreach (int i in Edad)
+//{
+//   Console.WriteLine(i);
+//}
+
+//ELIMINAR DE LA LISTA
+
+//Edad.Remove(10);
+//Console.WriteLine("\n");
+//foreach (int i in Edad)
+//{
+//    Console.WriteLine(i);
+//}
+
+//List<String> L = new List<String>() {"Perro", "Casa", "Cebra", "Tortuga",
+//"Burro", "Mar", "Dedo", "Tasa", "Nube", "Sol"};
+
+//L.Sort();
+
+//foreach (string i in L)
+//{
+//    Console.WriteLine(i);
+//} { }
+
+//var L = new[] { 1, 2, 3, 9.3 };
+//foreach (var x in L)
+//{ 
+//    Console.WriteLine(x);
+//}
+//Console.WriteLine(L.GetType());
+
+//var Persona = new {Nombre = "More", Edad = 20};
+//Console.WriteLine(Persona.Nombre);
+//Console.WriteLine(Persona.GetType());
+
